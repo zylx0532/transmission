70598bc0c4df8db6ff9663821bd5341ede4bc99f
Blood.Cold <webmaster@zhiqiang.name>
2019-09-27 21:44:36 +0800

    wget忽略证书修正

--- 2.94/transmissionbt.sh
+++ 2.94/transmissionbt.sh
@@ -15,14 +15,14 @@ mv -f /home/Downloads /home/transmission
 mv -f /home/resume /home/transmission/.config/transmission
 mv -f /home/torrents /home/transmission/.config/transmission
 cd /root
-wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/intltool-0.40.6.tar.gz -O intltool-0.40.6.tar.gz --no-check-certificate
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/intltool-0.40.6.tar.gz --no-check-certificate -O intltool-0.40.6.tar.gz
 tar zxf intltool-0.40.6.tar.gz
 cd intltool-0.40.6
 ./configure --prefix=/usr
 make -s
 make -s install
 cd ..
-wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/libevent-2.0.21-stable.tar.gz -O libevent-2.0.21-stable.tar.gz --no-check-certificate
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/libevent-2.0.21-stable.tar.gz --no-check-certificate -O libevent-2.0.21-stable.tar.gz
 tar zxf libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
 ./configure
@@ -35,7 +35,7 @@ ln -s /usr/lib/libevent-2.0.so.5 /usr/local/lib/libevent-2.0.so.5
 ln -s /usr/lib/libevent-2.0.so.5.1.9 /usr/local/lib/libevent-2.0.so.5.1.9
 echo install Transmisson
 cd /root
-wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/transmission-2.94.tar.xz -O transmission-2.94.tar.xz --no-check-certificate
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/transmission-2.94.tar.xz --no-check-certificate -O transmission-2.94.tar.xz
 tar Jxvf transmission-2.94.tar.xz
 cd transmission-2.94
 ./configure --prefix=/usr
@@ -43,19 +43,19 @@ make -s
 make -s install
 useradd -m transmission
 passwd -d transmission
-wget https://github.zhiqiang.name/zylx0532/transmission/master/2.94/transmission.sh -O /etc/init.d/transmissiond --no-check-certificate
+wget https://github.zhiqiang.name/zylx0532/transmission/master/2.94/transmission.sh --no-check-certificate -O /etc/init.d/transmissiond
 chmod 755 /etc/init.d/transmissiond
 chkconfig --add transmissiond
 chkconfig --level 2345 transmissiond on
 mkdir -p /home/transmission/Downloads/
 chmod g+w /home/transmission/Downloads/
-wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/settings.json -O settings.json --no-check-certificate
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/settings.json --no-check-certificate -O settings.json
 mkdir -p /home/transmission/.config/transmission/
 mv -f settings.json /home/transmission/.config/transmission/settings.json
 chown -R transmission.transmission /home/transmission
-wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/index.html -O index.html --no-check-certificate
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/index.html --no-check-certificate -O index.html
 mv -f index.html /usr/share/transmission/web/index.html
-wget -O webgui.sh https://github.zhiqiang.name/zylx0532/transmission/master/2.94/webgui.sh && bash webgui.sh --no-check-certificate
+wget -O webgui.sh https://github.zhiqiang.name/zylx0532/transmission/master/2.94/webgui.sh --no-check-certificate && bash webgui.sh
 service transmissiond start
 /sbin/iptables -I INPUT -p tcp --dport 9091 -j ACCEPT
 /sbin/iptables -I INPUT -p tcp --dport 65050 -j ACCEPT
