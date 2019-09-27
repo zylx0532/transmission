abaed9b3c0051ce06c8c3544cbd85d589e85b20f
Blood.Cold <webmaster@zhiqiang.name>
2019-09-27 20:07:11 +0800

    更改安装源至腾讯云加速

--- 2.94/webgui.sh
+++ 2.94/webgui.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yum -y install wget unzip
-wget -c https://github.com/zylx0532/transmission/master/2.94/index.html -O index.html
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/index.html -O index.html
 mv -f index.html /usr/share/transmission/web/index.original.html
 rm -rf /usr/share/transmission/web/index.html /usr/share/transmission/web/index.mobile.html /usr/share/transmission/web/tr-web-control
-wget -c https://github.com/zylx0532/transmission/master/2.94/webgui.zip -O webgui.zip
+wget -c https://github.zhiqiang.name/zylx0532/transmission/master/2.94/webgui.zip -O webgui.zip
 unzip -o webgui.zip
 cd webgui
 mv -f * /usr/share/transmission/web
